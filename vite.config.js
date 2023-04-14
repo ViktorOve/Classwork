bac4bb516da021647ef30a121feae2507e88fd92
ViktorOve <126905103+ViktorOve@users.noreply.github.com>
2023-04-14 12:11:07 -0600

    add classwork

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  build: {
+    lib: {
+      entry: 'src/my-element.js',
+      formats: ['es'],
+    },
+    rollupOptions: {
+      external: /^lit/,
+    },
+  },
 })
